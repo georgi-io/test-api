b0adb4204a38a9caf8988f1191175a6cd4f7509e
Sebastian Georgi <sebastian@georgi.io>
2021-12-14 09:32:19 +0100

    Another try of making AWS and GH actions work

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -24,11 +24,6 @@ const image = repositoryUrl.apply(r => r + ":" + deployVersion);
 //
 // export const url = lb.endpoint.hostname;
 
-// const deployUser = new aws.iam.User("test-api-deployment-user", {
-//   path: "/system/",
-//   tags: { "Name": "test-api-deployment-user"}
-// });
-// const deployUserAccessKey = new aws.iam.AccessKey("test-api-deployment-user-key", { user: deployUser.name });
 // const deployUserPolicy = new aws.iam.UserPolicy("test-api-deployment-user-policy", {
 //   user: deployUser.name,
 //   policy: {
@@ -48,6 +43,44 @@ const image = repositoryUrl.apply(r => r + ":" + deployVersion);
 //     }]
 //   },
 // });
-//
-// export const deployUserAccessKeyID = deployUserAccessKey.id
-// export const deployUserAccessKeySecret = deployUserAccessKey.encryptedSecret
+
+const deployRole = new aws.iam.Role("deploy-role", {
+  assumeRolePolicy: JSON.stringify({
+    Version: "2012-10-17",
+    Statement: [{
+      Action: "sts:AssumeRole",
+      Principal: {
+        Service: "ecs.amazonaws.com"
+      },
+      Effect: "Allow",
+      Sid: ""
+    }]
+  })
+});
+
+const deployRolePolicy = new aws.iam.RolePolicy("deploy-role-policy", {
+  role: deployRole,
+  policy: JSON.stringify({
+    Version: "2012-10-17",
+    Statement: [{
+      Action: [
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:CompleteLayerUpload",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
+      ],
+      Effect: "Allow",
+      Resource: "*",
+      Condition: {
+        StringEquals: {
+          "token.actions.githubusercontent.com:sub": "repo:georgi-io/test-api:ref:refs/heads/releases"
+        }
+      }
+    }]
+  })
+});
+
+export const deployRoleARN = deployRole.arn
