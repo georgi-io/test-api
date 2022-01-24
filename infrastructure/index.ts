9cf578d950acb0c2d11de8a1b33a50cd017f7d63
Sebastian Georgi <sebastian@georgi.io>
2022-01-24 13:05:37 +0100

    Tidied GH-Actions and Pulumi setup

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,13 +1,12 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 
-const deployVersion = "0.0.1-SNAPSHOT"
+const deployVersion = '0.0.1-SNAPSHOT'
 
-const repo = new awsx.ecr.Repository("test-api", {
+const repo = new awsx.ecr.Repository('test-api', {
   lifeCyclePolicyArgs: {
     rules: [{
-      selection: "any",
+      selection: 'any',
       maximumNumberOfImages: 3,
     }]
   }
@@ -15,60 +14,68 @@ const repo = new awsx.ecr.Repository("test-api", {
 export const repositoryUrl = repo.repository.repositoryUrl;
 
 
-// const image = repositoryUrl.apply(r => r + ":" + deployVersion);
-//
-// const lb = new awsx.lb.ApplicationListener("test-api-listener", { port: 9000, protocol: "HTTP" });
-// const testApiService = new awsx.ecs.FargateService("test-api-service", {
-//   taskDefinitionArgs: {
-//     containers: {
-//       testapi: {
-//         image: image,
-//         memory: 128,
-//         portMappings: [ lb ],
-//       },
-//     },
-//   },
-//   desiredCount: 1,
-// });
-//
-// export const url = lb.endpoint.hostname;
+const image = repositoryUrl.apply(r => r + ':' + deployVersion);
 
-const identityProvider = new aws.iam.OpenIdConnectProvider("github-oicd", {
-  clientIdLists: ["sts.amazonaws.com"],
-  thumbprintLists: ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"],
-  url: "https://token.actions.githubusercontent.com",
+const cluster = new awsx.ecs.Cluster('test-api-cluster');
+const alb = new awsx.elasticloadbalancingv2.ApplicationLoadBalancer(
+  'test-api--lb', {external: true, securityGroups: cluster.securityGroups});
+const atg = alb.createTargetGroup(
+  'test-api--tg', {port: 9000, protocol: 'HTTP', deregistrationDelay: 0});
+const web = atg.createListener('web', {port: 80});
+
+const appService = new awsx.ecs.FargateService('test-api--svc', {
+  cluster,
+  taskDefinitionArgs: {
+    containers: {
+      testapi: {
+        image: image,
+        memory: 128,
+        portMappings: [web],
+      },
+    }
+  },
+  desiredCount: 2,
+});
+``
+
+export const url = web.endpoint.hostname;
+
+const identityProvider = new aws.iam.OpenIdConnectProvider('github-oicd', {
+  clientIdLists: ['sts.amazonaws.com'],
+  thumbprintLists: ['a031c46782e6e6c662c2c87c76da9aa62ccabd8e'],
+  url: 'https://token.actions.githubusercontent.com',
 })
 
-const deployRole = new aws.iam.Role("deploy-role", {
-  assumeRolePolicy:  identityProvider.arn.apply((providerArn) => JSON.stringify({
-    Version: "2012-10-17",
+const deployRole = new aws.iam.Role('deploy-role', {
+  assumeRolePolicy: identityProvider.arn.apply((providerArn) => JSON.stringify({
+    Version: '2012-10-17',
     Statement: [{
-      Sid: "RoleForGithub",
-      Action: "sts:AssumeRoleWithWebIdentity",
-      Effect: "Allow",
-      Principal: { Federated: providerArn },
-      Condition: { StringLike: { "token.actions.githubusercontent.com:sub": `repo:georgi-io/test-api:*` } }
+      Sid: 'RoleForGithub',
+      Action: 'sts:AssumeRoleWithWebIdentity',
+      Effect: 'Allow',
+      Principal: {Federated: providerArn},
+      Condition: {StringLike: {'token.actions.githubusercontent.com:sub': `repo:georgi-io/test-api:*`}}
     }]
   }))
 })
 
-new aws.iam.RolePolicy("deploy-role-policy", {
+new aws.iam.RolePolicy('deploy-role-policy', {
   role: deployRole.id,
   policy: JSON.stringify({
-    Version: "2012-10-17",
+    Version: '2012-10-17',
     Statement: [{
       Action: [
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:CompleteLayerUpload",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:InitiateLayerUpload",
-        "ecr:PutImage",
-        "ecr:UploadLayerPart",
-        "ecr:GetAuthorizationToken"
+        'ecr:BatchGetImage',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:CompleteLayerUpload',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:InitiateLayerUpload',
+        'ecr:PutImage',
+        'ecr:UploadLayerPart',
+        'ecr:GetAuthorizationToken'
       ],
-      Effect: "Allow",
-      Resource: "*"
+      Effect: 'Allow',
+      Resource: '*'
     }]
   })
 });
